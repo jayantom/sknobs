d4b9431d878b8d4b4573926de1e03ca7aab3e237
Jay Minocha <jminocha@ridge.ba.provino-tech.com>
2017-09-22 13:44:36 -0700

    Update setup.py

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,8 +1,5 @@
 from distutils.core import setup, Extension
 setup(name='sknobs',
       version='1.0',
-      author='Warren Stapleton',
-      author_email='warren@sknobs.sourceforce.com',
-      url='sknobs.sourceforge.com',
       ext_modules=[Extension('sknobs', ['../c/sknobs.c', 'sknobs_ext.c'], include_dirs=['../c'])],
       )
